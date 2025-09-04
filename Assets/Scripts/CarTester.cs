b4e158493870d05f27ed16e354e8444a0bf0f22a
VidoTheWiz <mcgreg500@morrisville.edu>
2025-09-04 04:00:24 -0400

    Initial commit

---
+++ Assets/Scripts/CarTester.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarTester : MonoBehaviour
+{
+    // Start is called once before the first frame update
+    void Start()
+    {
+        // This is our test message!
+        Debug.Log("My awesome car is ready! Hello from ");
+    }
+}
+
